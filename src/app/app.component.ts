2e87f5516e55c07c5f30fe3071e1b8a40b39c8f2
Carlos Custodio <chcustodio97@gmail.com>
2022-04-20 15:08:08 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component,OnInit } from '@angular/core';
+import { count, filter, map, merge, Observable, of, Subscriber } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,48 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rxLab';
+  list:String[] = [];
+  elementsCounted=0;
+  elementsFiltered=0;
+  elementsMerged=0;
+  observable1 = new Observable()
+  observable2 = new Observable()
+  constructor( private http: HttpClient){
+  
+  }
+
+  observables(){
+    
+   this.observable1= new Observable( subscriber => {
+    subscriber.next("hello");
+    subscriber.next("how");
+    subscriber.next("are");
+    subscriber.next("you");
+
+    setTimeout(() =>{
+      subscriber.next("Good!");
+      subscriber.complete();
+    },1000);
+   
+  } );
+   this.observable1.subscribe(({
+   next: (v:any) => this.list.push(v),
+   error: (e) => console.error(e),
+}))
+  
+  }
+  counted(){
+    this.observable1.pipe(count()).subscribe(element => this.elementsCounted=element);
+  }
+  merged(){
+    this.observable2 = of(1,2,3,4,5);
+    merge(this.observable1,this.observable2).pipe(count()).subscribe(element=> this.elementsMerged=element)
+  }
+
+  filtered(){
+    merge(this.observable1,this.observable2).pipe(filter((r:any) => isNaN(r)),count()).subscribe(element=>this.elementsFiltered=element)
+    let observable3 = this.http.get("https://jsonplaceholder.typicode.com/posts");
+    
+  }
+  
 }
